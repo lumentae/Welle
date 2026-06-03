b819924a03d24206ec4d05f3672033075abd4c7f
lumentae <me@lumentae.dev>
2026-06-03 23:02:27 +0200

    Change song struct to use camelCase instead of snake_case

--- medialib/include/types/Song.h
+++ medialib/include/types/Song.h
@@ -2,48 +2,43 @@
 #include <string>
 #include <nlohmann/json.hpp>
 
+#include "Artist.h"
+
 namespace welle::medialib::types {
-
     struct Song {
-        struct Artist {
-            std::string id;
-            std::string name;
-        };
-        NLOHMANN_DEFINE_TYPE_INTRUSIVE(Artist, id, name)
-
         struct Genre {
             std::string name;
         };
         NLOHMANN_DEFINE_TYPE_INTRUSIVE(Genre, name)
 
         std::string album;
-        std::vector<Artist> album_artists;
-        std::string album_id;
+        std::vector<Artist> albumArtists;
+        std::string albumId;
         std::string artist;
-        std::string artist_id;
+        std::string artistId;
         std::vector<Artist> artists;
         std::string comment;
-        std::string content_type;
+        std::string contentType;
         std::vector<nlohmann::json> contributors;
-        std::string cover_art;
+        std::string coverArt;
         std::string created;
-        std::string display_album_artist;
-        std::string display_artist;
-        std::string display_composer;
+        std::string displayAlbumArtist;
+        std::string displayArtist;
+        std::string displayComposer;
         int64_t duration;
         std::string genre;
         std::vector<Genre> genres;
         std::string id;
         std::string parent;
         std::string path;
-        int64_t play_count;
+        int64_t playCount;
         std::string played;
-        int64_t sampling_rate;
-        std::string sort_name;
+        int64_t samplingRate;
+        std::string sortName;
         std::string suffix;
         std::string title;
         std::string type;
         int64_t year;
     };
-    NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_WITH_DEFAULT(Song, album, album_artists, album_id, artist, artist_id, artists, comment, content_type, contributors, cover_art, created, display_album_artist, display_artist, display_composer, duration, genre, genres, id, parent, path, play_count, played, sampling_rate, sort_name, suffix, title, type, year)
+    NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_WITH_DEFAULT(Song, album, albumArtists, albumId, artist, artistId, artists, comment, contentType, contributors, coverArt, created, displayAlbumArtist, displayArtist, displayComposer, duration, genre, genres, id, parent, path, playCount, played, samplingRate, sortName, suffix, title, type, year)
 }
--- welle/src/model/SongModel.cpp
+++ welle/src/model/SongModel.cpp
@@ -17,12 +17,12 @@ namespace welle::model {
             case TitleRole: return QString::fromStdString(song.title);
             case ArtistRole: return QString::fromStdString(song.artist);
             case AlbumRole: return QString::fromStdString(song.album);
-            case AlbumIdRole: return QString::fromStdString(song.album_id);
+            case AlbumIdRole: return QString::fromStdString(song.albumId);
             case DurationRole: return static_cast<qlonglong>(song.duration);
-            case CoverArtRole: return QString::fromStdString(song.cover_art);
+            case CoverArtRole: return QString::fromStdString(song.coverArt);
             case PathRole: return QString::fromStdString(song.path);
             case SuffixRole: return QString::fromStdString(song.suffix);
-            case PlayCountRole: return static_cast<qlonglong>(song.play_count);
+            case PlayCountRole: return static_cast<qlonglong>(song.playCount);
             default: return {};
         }
     }
